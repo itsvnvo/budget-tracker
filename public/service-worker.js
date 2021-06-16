9524b6a4cc3e7f696af7f1085067724d1832b386
Vincent Vo <itsvnvo@gmail.com>
2021-06-15 22:59:00 -0500

    removed event respond with

--- public/service-worker.js
+++ public/service-worker.js
@@ -58,11 +58,5 @@ self.addEventListener('fetch', (event) => {
             })
         );
     }
-
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
 });
 
